8ce44f5017ad51344cdf83bf13b74fef11347859
sairamyadhav <84068020+sairamyadhav@users.noreply.github.com>
2022-06-08 21:10:45 +0530

    Add files via upload

---
+++ sorting-visualizer/scripts/app.js
@@ -0,0 +1,74 @@
+"use strict";
+const start = async () => {
+  let algoValue = Number(document.querySelector(".algo-menu").value);
+  let speedValue = Number(document.querySelector(".speed-menu").value);
+  let sizeValue = Number(document.querySelector(".size-menu").value)
+
+  if (speedValue === 0) {
+    speedValue = 1;
+  }
+  if (algoValue === 0) {
+    alert("No Algorithm Selected");
+    return;
+  }
+  if (sizeValue == 0) {
+    alert("Select Array Size");
+    return
+  }
+
+  let algorithm = new sortAlgorithms(speedValue);
+  if (algoValue === 1) await algorithm.BubbleSort();
+  if (algoValue === 2) await algorithm.SelectionSort();
+  if (algoValue === 3) await algorithm.InsertionSort();
+  if (algoValue === 4) await algorithm.MergeSort();
+  if (algoValue === 5) await algorithm.QuickSort();
+};
+
+const LoadScreen = async () => {
+  await RandomList();
+};
+
+const RandomList = async () => {
+  let sizeValue = Number(document.querySelector(".size-menu").value);
+  await clearScreen();
+
+  let list = await randomList(sizeValue);
+  const arrayNode = document.querySelector(".array");
+  console.log(arrayNode);
+  console.log(list);
+  for (const element of list) {
+    const node = document.createElement("div");
+    node.className = "cell";
+    node.setAttribute("value", String(element));
+    node.style.height = `${3.8 * element}px`;
+    const p = document.createElement("p");
+    const text = document.createTextNode(String(element));
+    p.appendChild(text);
+    p.setAttribute("class", "cellP");
+    node.appendChild(p);
+    arrayNode.appendChild(node);
+  }
+};
+
+const randomList = async (Length) => {
+  let list = new Array();
+  let lowerBound = 1;
+  let upperBound = 100;
+
+  for (let counter = 0; counter < Length; ++counter) {
+    let randomNumber = Math.floor(
+      Math.random() * (upperBound) + lowerBound
+    );
+    list.push(parseInt(randomNumber));
+  }
+  return list;
+};
+
+const clearScreen = async () => {
+  document.querySelector(".array").innerHTML = "";
+};
+
+document.querySelector(".start").addEventListener("click", start);
+document.querySelector(".size-menu").addEventListener("change", LoadScreen);
+document.querySelector(".algo-menu").addEventListener("change", LoadScreen);
+window.onload = LoadScreen;
